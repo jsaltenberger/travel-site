d5b999baa257a815f0e33488490f347aa02e3165
Joe <j.saltenberger@gmail.com>
2017-11-22 13:55:14 -0800

    Added postcss processing.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
 
 gulp.task('html', function() {
     console.log('HTML task here...');
 });
 
 gulp.task('styles', function() {
-    console.log('CSS task here...');
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {

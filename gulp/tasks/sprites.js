121be463ecb3d9388f74a9d8d753c4fef276566b
Joe <j.saltenberger@gmail.com>
2017-12-11 14:59:45 -0800

    Completed automatic Sprites.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
+var del = require('del');
 
 var config = {
     mode: {
@@ -14,7 +15,11 @@ var config = {
     }
 }
 
-gulp.task('createSprite', function() {
+gulp.task('beginClean', function() {
+    return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function() {
     return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))    
     .pipe(gulp.dest('./app/temp/sprite/'));
@@ -30,4 +35,8 @@ gulp.task('copySpriteCSS', ['createSprite'], function() {
         .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
+    return del('./app/temp/sprite');
+});
+
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
